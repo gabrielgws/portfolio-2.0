039ca1c5808f4db48bf18962b24f268370934fed
gabrielgws.dev <gabrielgws.dev@gmail.com>
2023-10-04 20:02:16 -0300

    feat: Knowledge session

---
+++ app/components/pages/home/know-techs/index.tsx
@@ -0,0 +1,21 @@
+import { SectionTitle } from "@/app/components/section-title"
+import { KnowTech } from "./know-tech"
+import { TbBrandNextjs } from "react-icons/tb"
+
+export const KnowTechs = () => {
+  return (
+    <section className="container py-16">
+      <SectionTitle subtitle="Competências" title="Conhecimentos" />
+
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
+        {Array.from({ length: 8}).map((_, index) => (
+          <KnowTech key={index} tech={{
+            icon: <TbBrandNextjs />,
+            name: 'Next.js',
+            startDate: '2021-01-01'
+          }} />
+        ))}
+      </div>
+    </section>
+  )
+}
