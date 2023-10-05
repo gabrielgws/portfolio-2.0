8282778555efd5dd4e863cadf9f8e7d5332bd38b
gabrielgws.dev <gabrielgws.dev@gmail.com>
2023-10-04 21:58:34 -0300

    feat: featured projects session

---
+++ app/components/link/index.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from 'react'
+import NextLink from 'next/link'
+import { cn } from '@/app/lib/utils'
+
+type LinkProps = ComponentProps<typeof NextLink>
+
+export const Link = ({ className, children, ...props }: LinkProps) => {
+  return (
+    <NextLink className={cn(
+      'flex items-center gap-2 text-gray-300 text-sm hover:text-emerald-500 transition-colors',
+      className
+    )} {...props}>
+      {children}
+    </NextLink>
+  )
+}
